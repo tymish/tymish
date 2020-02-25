83ced5afbf6e12cfe485e095cc8e068b0404e2a0
Hubert Lin <linhub15@gmail.com>
2020-02-25 16:39:02 -0700

    implement create time report command

--- Application/TimeReports/Commands/CreateTimeReport.cs
+++ Application/TimeReports/Commands/CreateTimeReport.cs
@@ -4,21 +4,54 @@ using System.Threading.Tasks;
 using Tymish.Domain.Entities;
 using MediatR;
 using System.Collections.Generic;
+using Tymish.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Tymish.Application.Exceptions;
 
 namespace Tymish.Application.TimeReports.Commands
 {
     public class CreateTimeReportCommand : IRequest<TimeReport>
     {
-        public DateTime EmployeeNumber { get; set; }
+        public int EmployeeNumber { get; set; }
         public IList<TimeEntry> TimeEntries { get; set; }
     }
 
     public class CreateTimeReportHandler : IRequestHandler<CreateTimeReportCommand, TimeReport>
     {
-        public CreateTimeReportHandler() {}
-        public Task<TimeReport> Handle(CreateTimeReportCommand request, CancellationToken cancellationToken)
+        private readonly ITymishDbContext _context;
+
+        public CreateTimeReportHandler(ITymishDbContext context) {
+            _context = context;
+        }
+        public async Task<TimeReport> Handle(CreateTimeReportCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var employee = await _context
+                .Set<Employee>()
+                .SingleOrDefaultAsync(
+                    e => e.EmployeeNumber == request.EmployeeNumber,
+                    cancellationToken
+                );
+            
+            if (employee == default(Employee))
+            {
+                throw new NotFoundException(nameof(Employee), request.EmployeeNumber);
+            }
+
+            var entity = new TimeReport
+            {
+                Id = new Guid(),
+                Issued = default(DateTime),
+                Submitted = default(DateTime),
+                Paid = default(DateTime),
+                TimeEntries = request.TimeEntries,
+                Employee = employee
+            };
+
+            _context.Set<TimeReport>().Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+            
+            return entity;
         }
     }
 }

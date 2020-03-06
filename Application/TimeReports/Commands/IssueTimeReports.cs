74810377b4e468cb34b31fb3892056d7d83d31c6
Hubert Lin <linhub15@gmail.com>
2020-03-05 20:09:56 -0700

    issue time report based on issue date

--- Application/TimeReports/Commands/IssueTimeReports.cs
+++ Application/TimeReports/Commands/IssueTimeReports.cs
@@ -13,7 +13,7 @@ namespace Tymish.Application.TimeReports.Commands
 {
     public class IssueTimeReportsCommand : IRequest
     {
-        public IList<int> EmployeeNumbers { get; set; }
+        public DateTime Issued { get; set; }
     }
 
     public class IssueTimeReportsHandler : IRequestHandler<IssueTimeReportsCommand, Unit>
@@ -27,22 +27,47 @@ namespace Tymish.Application.TimeReports.Commands
         }
         public async Task<Unit> Handle(IssueTimeReportsCommand request, CancellationToken cancellationToken)
         {
-            // Get time reports for an employeeNumber
-            var timeReports = await _context.Set<TimeReport>()
-                .Where(e => request.EmployeeNumbers
-                    .Any(num => e.Employee.EmployeeNumber == num)
-                )
+            var employees = await _context.Set<Employee>()
+                .Include(x => x.TimeReports)
                 .ToListAsync(cancellationToken);
 
-            // Set Issued Date
-            timeReports = timeReports.Select(e => {
-                e.Issued = DateTime.UtcNow;
-                return e;
-            }).ToList();
+            var emailList = new List<(string, Guid)>();
 
-            _context.Set<TimeReport>().UpdateRange(timeReports);
+            foreach(var employee in employees)
+            {
+                // Check if they have a time report for this month
+                var alreadyHasTimeReportForThisMonth = employee.TimeReports.Any(x
+                    => x.Issued.Month == request.Issued.Month
+                    && x.Issued.Year == request.Issued.Year
+                );
+
+                if (alreadyHasTimeReportForThisMonth)
+                {
+                    continue; // Skip this employee
+                }
+                
+                var timeReportId = new Guid();
+
+                emailList.Add((employee.Email, timeReportId));
+
+                employee.TimeReports.Add(new TimeReport()
+                {
+                    Id = timeReportId,
+                    EmployeeId = employee.Id,
+                    Issued = DateTime.UtcNow
+                });
+            }
 
             await _context.SaveChangesAsync(cancellationToken);
+
+            // Send email to the employees
+            foreach(var email in emailList)
+            {
+                var address = email.Item1;
+                var timeSheetUrl = ""; // makeUrl(email.Item2);
+                // Send email with link to their most recent 
+                // smptGateway.Send(address, timeSheetUrl);
+            }
 
             return Unit.Value;
         }
--- Domain/Entities/Employee.cs
+++ Domain/Entities/Employee.cs
@@ -11,13 +11,14 @@ namespace Tymish.Domain.Entities
         public string FamilyName { get; set; }
         public string Email { get; set; }
         public Decimal HourlyPay { get; set; }
-        public virtual IList<TimeReport>? TimeReports { get; set; }
+        public virtual IList<TimeReport> TimeReports { get; set; }
 
         public Employee()
         {
             GivenName = string.Empty;
             FamilyName = string.Empty;
             Email = string.Empty;
+            TimeReports = new List<TimeReport>();
         }
     }
 }

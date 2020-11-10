c62ffc9e09230f4019f147e002bc26ed72772762
Hubert Lin <linhub15@gmail.com>
2020-11-09 18:49:56 -0700

    create a seed admin user

--- Persistence/TymishDbContext.cs
+++ Persistence/TymishDbContext.cs
@@ -1,6 +1,7 @@
 using Tymish.Domain.Entities;
 using Tymish.Application.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Tymish.Persistence
 {
@@ -18,6 +19,10 @@ namespace Tymish.Persistence
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.ApplyConfigurationsFromAssembly(typeof(TymishDbContext).Assembly);
+
+            builder.Entity<User>().HasData(
+                new User {Id = Guid.NewGuid(), Email = "admin@tymish.com", Password="test"}
+            );
         }
     }
 }
